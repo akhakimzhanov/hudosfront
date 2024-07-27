332962d3a7464c5df29b9a1d3f73355b428d369b
akhakimzhanov <akhakimzhanov@gmail.com>
2024-07-27 19:33:10 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+import './src/index.css'
+import './src/js/swiper'
+import './src/js/custome'
+import './src/js/accordion'
+import './src/js/animation'
+import './src/js/petite-vue'
+import './src/js/item-animation'
---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        price: 'price.html',
+        notFound: 'not-found.html',
+        portfolio: 'portfolio.html',
+        item: 'item.html',
+      },
+    },
+  },
+})
